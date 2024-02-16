5a19ca59699f0b26a0d775e16d8a2857e689bd07
Mouhlal <akramemouhlal@gmail.com>
2024-02-16 19:37:12 +0100

    nothing new

--- src/Contact/Header.jsx
+++ src/Contact/Header.jsx
@@ -4,29 +4,27 @@ const Header = () => {
   return (
     <div className="text-center">
       <h2 className="text-center pt-2">
-      <i className="bi bi-journal-text"></i> Gestion des Contacts
+        <i className="bi bi-journal-text"></i> Gestion des Contacts
       </h2>
-          <br />
-      <h4 style={{display:"flex" , justifyContent:"space-around"}}>
+      <br />
+      <h4 style={{ display: "flex", justifyContent: "space-around" }}>
         <Link to="/" style={{ textDecoration: "none" }}>
           <i className="bi bi-house-door-fill"></i> Home
         </Link>
-
 
         <Link to="/Contact" style={{ textDecoration: "none" }}>
           <i className="bi bi-person-lines-fill"></i> Contact
         </Link>
 
-        
         <Link style={{ textDecoration: "none" }} to="/add">
           <i className="bi bi-person-plus-fill"></i> Add Contact
         </Link>
-        
 
         <Link to="/search" style={{ textDecoration: "none" }}>
-        <i className="bi bi-search"></i>  Search Contact
+          <i className="bi bi-search"></i> Search Contact
         </Link>
       </h4>
+      <br />
     </div>
   );
 };
